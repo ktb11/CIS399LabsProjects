5eefdb21d71e08f162017f1b5704b6f4c9cd1bdc
Kyle Bennett <ktb@uoregon.edu>
2018-06-30 15:22:37 -0700

    updating buttons

--- Lab2/app/src/main/java/com/example/kyle/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/kyle/lab2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     private Button endTurn;
     private TextView turnPoints;
     private int curPoints = 0;
+    private int rollVal;
+    private ImageView DieImage;
+    private TextView player1Score;
 
     private Random rand = new Random();
 
@@ -27,20 +31,61 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         rollDie = (Button)findViewById(R.id.rollBtn);
         endTurn = (Button)findViewById(R.id.endTurnBtn);
         turnPoints = (TextView)findViewById(R.id.turnPointsId);
+        DieImage = (ImageView)findViewById(R.id.dieImgId);
+        player1Score = (TextView)findViewById(R.id.player1ScoreId);
 
         // set listeners
         rollDie.setOnClickListener(this);
+        endTurn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        int id = 0;
+
         switch (v.getId()) {
             case R.id.rollBtn:
-                curPoints += (rand.nextInt(6)+1);
+
+                rollVal = (rand.nextInt(6)+1);
+                if (rollVal == 1) {
+                    id = R.drawable.die1;
+                    curPoints += 1;
+                }
+
+                if (rollVal == 2) {
+                    id = R.drawable.die2;
+                    curPoints += 2;
+                }
+
+                if (rollVal == 3){
+                    id = R.drawable.die3;
+                    curPoints += 3;
+                }
+
+                if (rollVal == 4){
+                    id = R.drawable.die4;
+                    curPoints += 4;
+                }
+
+                if (rollVal == 5){
+                    id = R.drawable.die5;
+                    curPoints += 5;
+                }
+
+                if (rollVal == 6){
+                    id = R.drawable.die6;
+                    curPoints += 6;
+                }
+
 
                 break;
             case R.id.endTurnBtn:
+                player1Score.setText(Integer.toString(curPoints));
+                curPoints = 0;
+
                 break;
         }
+        DieImage.setImageResource(id);
+        turnPoints.setText(Integer.toString(curPoints));
     }
 }
