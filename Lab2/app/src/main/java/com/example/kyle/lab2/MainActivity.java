af384094b24ef4f9e84b20e9dabdd3ccb20b1d46
Kyle Bennett <ktb@uoregon.edu>
2018-06-30 13:07:43 -0700

    updating

--- Lab2/app/src/main/java/com/example/kyle/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/kyle/lab2/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.kyle.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    // define variables for widgets
+    private Button rollDie;
+    private Button endTurn;
+    private TextView turnPoints;
+    private int curPoints = 0;
+
+    private Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollDie = (Button)findViewById(R.id.rollBtn);
+        endTurn = (Button)findViewById(R.id.endTurnBtn);
+        turnPoints = (TextView)findViewById(R.id.turnPointsId);
+
+        // set listeners
+        rollDie.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rollBtn:
+                curPoints += (rand.nextInt(6)+1);
+
+                break;
+            case R.id.endTurnBtn:
+                break;
+        }
     }
 }
