c35114ba66300b78c363c1e69a16017896a85f92
Kyle Bennett <ktb@uoregon.edu>
2018-07-09 12:43:05 -0700

    adding more to Lab4

--- Lab4/app/src/main/java/com/example/kyle/lab4/FirstFragment.java
+++ Lab4/app/src/main/java/com/example/kyle/lab4/FirstFragment.java
@@ -15,7 +15,8 @@ public class FirstFragment extends Fragment implements OnClickListener {
 
     private EditText player1Name;
     private EditText player2Name;
-    //private FirstActivity activity;
+    private FirstActivity activity;
+    private boolean twoPaneLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,10 +31,29 @@ public class FirstFragment extends Fragment implements OnClickListener {
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Get a references from the host activity
+        activity = (FirstActivity)getActivity();
+        player1Name = (EditText) activity.findViewById(R.id.player1NameId);
+        player2Name = (EditText) activity.findViewById(R.id.player2NameId);
+
+
+        // Check to see if FirstActivity has loaded a single or dual pane layout
+        //twoPaneLayout = activity.findViewById(R.id.second_fragment) != null;
+    }
+
+    @Override
     public void onClick(View v) {
         if (v.getId() == R.id.startGameBtn) {
+            String p1Name = player1Name.getText().toString();
+            String p2Name = player2Name.getText().toString();
+
 
             Intent intent = new Intent(getActivity(), SecondActivity.class);
+            intent.putExtra("p1Name", p1Name);
+            intent.putExtra("p2Name", p2Name);
             startActivity(intent);
 
         }
